4841daa71144fbbb741b40c91b2f4f8fcb548190
Joe Schweitzer <joeschweitzeros@gmail.com>
2012-06-08 21:03:43 -0400

    Initial ext-grails project with SlowCount domain class examples

---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,12 @@
+class UrlMappings {
+
+	static mappings = {
+		"/$controller/$action?/$id?"{
+			constraints {
+				// apply constraints here
+			}
+		}
+
+		"/"(uri:"/index.html")
+	}
+}
---
+++ grails-app/domain/joeschweitzer/SlowCount.groovy
@@ -0,0 +1,24 @@
+package joeschweitzer
+
+/**
+ * Domain class representing a class mapped to a really big table or view
+ * which does not return a total count quickly.
+ * 
+ * When this class is modified, HibernatePluginSupport will call count() which 
+ * could potentially hang our auto reload and also make our DBAs unhappy.  
+ * 
+ * We can potentially just override count() in classes like these to return 
+ * something like 0 or -1 and assume that developers know they should not be 
+ * calling count() on a domain class like this.
+ */
+class SlowCount {
+	
+	static mapping = {
+		table 'really_big_table_or_view'
+	}
+	
+	static int count() {
+		println 'Slow count called'
+		return 0
+	}
+}
